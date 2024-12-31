eff35b2819db1a44d07fba00eef0e9d0b0dded2c
Sithunimna <149657403+Sithunimna@users.noreply.github.com>
2024-12-31 18:07:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "EJRSmRJK#QtMN5Tw-Hq6yB9UfhA11YDkACR340Q-0brDwmdMubp4",
+SESSION_ID: process.env.SESSION_ID || "tdQGGCpK#sLn_wnI8HcnUleIuzJqqx67lpeVG6IjzJHioFRLsDqA",
 ALIVE_REACT: process.env.ALIVE_REACT || "👋",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://pomf2.lain.la/f/pkuujaup.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "KAVI EXE_V1_Connected ☑️_📂𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 KAVI_EXE📘☀",
@@ -13,5 +13,5 @@ AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
- AUTO_RECORDING: process.env.AUTO_RECORDING || "true",   
+ AUTO_RECORDING: process.env.AUTO_RECORDING || "false",   
 };
